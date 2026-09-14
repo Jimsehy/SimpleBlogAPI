ecf995a052737013ecd448c2e8bbf9b0eb798fc6
Jimsehy <theguy3.768@gmail.com>
2026-09-14 20:49:44 +1200

    anotherone

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from ckeditor.fields import RichTextField
+from django.utils import timezone
+
+from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -16,12 +23,12 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    header_image = models.ImageField(upload_to='images/', blank=True)
+    header_image = models.ImageField(upload_to='images/', blank=True, null=True)
     title_tag = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField()
     post_date = models.DateTimeField(auto_now_add=True)
-    snippet = models.CharField(max_length=200)
+    snippet = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes', blank=True)
 
@@ -31,10 +38,10 @@ class Post(models.Model):
     def published_time(self):
         db_time = self.post_date
         now = timezone.now()
-        seconds = (now - db_time).total_seconds()
+        seconds = abs(now - db_time).total_seconds()
         return seconds
 
-class Comment (models.Model):
+class Comment(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     body = models.TextField()
@@ -45,9 +52,7 @@ class Comment (models.Model):
 class Profile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
-    profile_picture = models.ImageField(upload_to='images/', blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='images/profile/',
+                                    blank=True, null=True)
     website = models.URLField(blank=True, null=True)
     github = models.URLField(blank=True, null=True)
-
-    def __str__(self):
-        return self.user.username
---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from blog.views import home
+from blog.viewsets import PostViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+
+urlpatterns = [
+    path('', home, name='home'),
+] + router.urls
